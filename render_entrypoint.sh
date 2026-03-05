692feac3d10e4fa7b05db546033968d87bab9002
CARLOS RICCI Roberto <roberto.ricci@sonepar.com.br>
2026-03-05 12:22:10 -0300

    ajustes

--- render_entrypoint.sh
+++ render_entrypoint.sh
@@ -30,6 +30,55 @@
 # exec gunicorn -w 2 -b 0.0.0.0:$PORT "wsgi:app"
 
 
+# #!/bin/bash
+# set -e
+
+# echo "Inicializando migrações..."
+# if [ ! -d "migrations" ]; then
+#     flask db init
+# fi
+
+# echo "Gerando migração..."
+# flask db migrate -m "init" || true
+
+# echo "Aplicando migrações..."
+# flask db upgrade || {
+
+#     echo "Conflito alembic detectado..."
+
+#     python <<EOF
+# import os
+# import psycopg
+
+# url = os.environ["DATABASE_URL"]
+
+# # remover driver do SQLAlchemy
+# if url.startswith("postgresql+psycopg://"):
+#     url = url.replace("postgresql+psycopg://", "postgresql://", 1)
+
+# if url.startswith("postgres://"):
+#     url = url.replace("postgres://", "postgresql://", 1)
+
+# conn = psycopg.connect(url)
+# conn.execute("DROP TABLE IF EXISTS alembic_version;")
+# conn.close()
+
+# print("Tabela alembic_version removida.")
+# EOF
+
+#     flask db upgrade
+# }
+
+# echo "🌱 make inicial..."
+
+# python make_admin.py
+
+# echo "Iniciando servidor..."
+
+# exec gunicorn -w 2 -b 0.0.0.0:$PORT wsgi:app
+
+
+
 #!/bin/bash
 set -e
 
@@ -46,18 +95,14 @@ flask db upgrade || {
 
     echo "Conflito alembic detectado..."
 
-    python <<EOF
+python <<EOF
 import os
 import psycopg
 
 url = os.environ["DATABASE_URL"]
 
-# remover driver do SQLAlchemy
 if url.startswith("postgresql+psycopg://"):
     url = url.replace("postgresql+psycopg://", "postgresql://", 1)
-
-if url.startswith("postgres://"):
-    url = url.replace("postgres://", "postgresql://", 1)
 
 conn = psycopg.connect(url)
 conn.execute("DROP TABLE IF EXISTS alembic_version;")
@@ -66,13 +111,12 @@ conn.close()
 print("Tabela alembic_version removida.")
 EOF
 
+    flask db stamp head
     flask db upgrade
 }
 
-echo "🌱 make inicial..."
-
-python make_admin.py
+echo "Criando admin..."
+python make_admin.py || true
 
 echo "Iniciando servidor..."
-
 exec gunicorn -w 2 -b 0.0.0.0:$PORT wsgi:app

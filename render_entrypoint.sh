ff01f0764fb54f097fa17ea88d91716e71df47ac
CARLOS RICCI Roberto <roberto.ricci@sonepar.com.br>
2026-03-05 11:51:22 -0300

    ajustes

--- render_entrypoint.sh
+++ render_entrypoint.sh
@@ -1,3 +1,35 @@
+# #!/bin/bash
+# set -e
+
+# echo "Inicializando migrações..."
+# if [ ! -d "migrations" ]; then
+#     flask db init
+# fi
+
+# echo "Gerando migração..."
+# flask db migrate -m "init" 2>/dev/null || true
+
+# echo "Aplicando migrações..."
+# flask db upgrade 2>/tmp/migrate_err.txt || {
+#     echo "Conflito de revisão detectado, resetando..."
+#     python -c "
+# import psycopg, os
+# conn = psycopg.connect(os.environ['DATABASE_URL'])
+# conn.autocommit = True
+# conn.execute('DROP TABLE IF EXISTS alembic_version;')
+# conn.close()
+# print('Tabela alembic_version removida.')
+# "
+#     flask db upgrade
+# }
+
+# echo "Make..."
+# ##python make_admin.py
+
+# echo "Iniciando servidor..."
+# exec gunicorn -w 2 -b 0.0.0.0:$PORT "wsgi:app"
+
+
 #!/bin/bash
 set -e
 
@@ -7,24 +39,32 @@ if [ ! -d "migrations" ]; then
 fi
 
 echo "Gerando migração..."
-flask db migrate -m "init" 2>/dev/null || true
+flask db migrate -m "init" || true
 
 echo "Aplicando migrações..."
-flask db upgrade 2>/tmp/migrate_err.txt || {
-    echo "Conflito de revisão detectado, resetando..."
-    python -c "
-import psycopg, os
-conn = psycopg.connect(os.environ['DATABASE_URL'])
-conn.autocommit = True
-conn.execute('DROP TABLE IF EXISTS alembic_version;')
+flask db upgrade || {
+
+    echo "Conflito alembic detectado..."
+
+    python <<EOF
+import os
+import psycopg
+
+url = os.environ["DATABASE_URL"]
+
+if url.startswith("postgres://"):
+    url = url.replace("postgres://", "postgresql://", 1)
+
+conn = psycopg.connect(url)
+conn.execute("DROP TABLE IF EXISTS alembic_version;")
 conn.close()
-print('Tabela alembic_version removida.')
-"
+
+print("Tabela alembic_version removida.")
+EOF
+
     flask db upgrade
 }
 
-echo "Make..."
-##python make_admin.py
-
 echo "Iniciando servidor..."
-exec gunicorn -w 2 -b 0.0.0.0:$PORT "wsgi:app"
+
+exec gunicorn -w 2 -b 0.0.0.0:$PORT wsgi:app
